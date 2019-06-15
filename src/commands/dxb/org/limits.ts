3eb7d625eec3b9fafc9518b1e1edb68bf62656c0
David Browaeys <david.browaeys@nab.com.au>
2019-06-15 10:12:35 +1000

    add new query explain cmd and update limit cmd

--- src/commands/dxb/org/limits.ts
+++ src/commands/dxb/org/limits.ts
@@ -2,9 +2,8 @@ import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
 
+const request = require('request');
 const exec = require('child_process').execSync;
-const path = require('path');
-const fs = require('fs');
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -34,72 +33,98 @@ export default class MetadataReset extends SfdxCommand {
   protected static requiresProject = false;
 
   public async run() {
+      console.log('Connecting to org...');
 			let accessToken = this.org.getConnection().accessToken;
-			let instanceUrl = this.org.getConnection().instanceUrl;
-      console.log('Connecting to ',instanceUrl,'...\n',accessToken);
-			var output = JSON.parse(exec(`curl \"${instanceUrl}/services/data/v45.0/limits/\" -H \"Authorization: Bearer ${accessToken}\"`).toString());
+      let instanceUrl = this.org.getConnection().instanceUrl;
       
-      var Table = require('tty-table');
-      var chalk = require('chalk');
-      var header = [
-        {
-          value : "Item",
-          color : "white", 
-          align : "left",
-          paddingLeft : 5,
-          width : 50
-        },
-        {
-          value : "Remaining",
-          color : "white", 
-          width : 20,
-          paddingLeft : 5,
-          align : "left"
-        },
-        {
-          value : "Max",
-          color : "white", 
-          width : 20,
-          paddingLeft : 5,
-          align : "left"
-        },
-        {
-          value : "Percentage",
-          color : "white", 
-          width : 30,
-          paddingLeft: 5,
-          align: "left",
-          formatter : function(value){
-            var str = value.toFixed(2) + '%';
-            if(value < 10){
-              str = chalk.black.red(str);
-            }else if(value < 30){
-              str = chalk.black.yellow(str);
-            }
-            return str;
-          }
-        }
-      ];
-
-
-      var rows = [];
-      for (var i in output){
-        rows.push([
-            i,
-            output[i].Remaining || 'N/A',
-            output[i].Max || 'N/A',
-            (output[i].Remaining / output[i].Max * 100)
-        ]);
+      if (!accessToken || !instanceUrl){
+        throw new SfdxError(`Connection not valid.`);
       }
-      var t1 = Table(header,rows,null,{
-        borderStyle : 1,
-        borderColor : "blue",
-        paddingBottom : 0,
-        headerAlign : "center",
-        align : "center",
-        color : "white",
-        truncate: "..."
-      });
-      console.log(t1.render());
-  }
+
+      console.log('Connected to ',instanceUrl,'...\n',accessToken);
+
+      const options = {
+          method  : 'GET',
+          headers : {
+                      'Content-Type' : 'application/json; charset=UTF-8',
+                      'Accept' : 'application/json',
+                      'Authorization' : 'Bearer ' + accessToken,
+                      'X-SFDC-Session' : accessToken
+                    },
+          url     : instanceUrl+'/services/data/v45.0/limits',
+          json: true,
+        };
+        request(options,
+          function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                console.log(body);
+                var Table = require('tty-table');
+                var chalk = require('chalk');
+                var header = [
+                  {
+                    value : "Item",
+                    color : "white", 
+                    align : "left",
+                    paddingLeft : 5,
+                    width : 50
+                  },
+                  {
+                    value : "Remaining",
+                    color : "white", 
+                    width : 20,
+                    paddingLeft : 5,
+                    align : "left"
+                  },
+                  {
+                    value : "Max",
+                    color : "white", 
+                    width : 20,
+                    paddingLeft : 5,
+                    align : "left"
+                  },
+                  {
+                    value : "Percentage",
+                    color : "white", 
+                    width : 30,
+                    paddingLeft: 5,
+                    align: "left",
+                    formatter : function(value){
+                      var str = value.toFixed(2) + '%';
+                      if(value < 10){
+                        str = chalk.black.red(str);
+                      }else if(value < 30){
+                        str = chalk.black.yellow(str);
+                      }
+                      return str;
+                    }
+                  }
+                ];
+
+
+                var rows = [];
+                for (var i in body){
+                  rows.push([
+                      i,
+                      body[i].Remaining || 'N/A',
+                      body[i].Max || 'N/A',
+                      (body[i].Remaining / body[i].Max * 100)
+                  ]);
+                }
+                var t1 = Table(header,rows,null,{
+                  borderStyle : 1,
+                  borderColor : "blue",
+                  paddingBottom : 0,
+                  headerAlign : "center",
+                  align : "center",
+                  color : "white",
+                  truncate: "..."
+                });
+                console.log(t1.render());
+              }else{
+                console.log('Unexpected Error!');
+                console.log(response);
+              }
+          }
+        );
+    }
 }

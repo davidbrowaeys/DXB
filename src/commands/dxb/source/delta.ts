4518dd838a37177951c03ee94719924f7902f23c
David Browaeys <dbrowaeys@deloitte.com.au>
2020-05-08 13:11:58 +1000

    minor fix

--- src/commands/dxb/source/delta.ts
+++ src/commands/dxb/source/delta.ts
@@ -68,7 +68,7 @@ export default class extends SfdxCommand {
     }
     let deployOutput = '';
     if (deltaMeta && deltaMeta.length > 0) {
-      deployOutput += `-p ${deltaMeta.join(',').replace(/(\s+)/g, '\\$1')}`;
+      deployOutput += `-p ${deltaMeta.join(',')}`;
     } else {
       deployOutput += `-p ${basedir}`;
     }

c6cfbb946050b36648a3030ee768a8e53885e2e1
David Browaeys <dbrowaeys@deloitte.com.au>
2020-05-12 21:47:05 +1000

    escape double quote

--- src/commands/dxb/source/delta.ts
+++ src/commands/dxb/source/delta.ts
@@ -68,7 +68,7 @@ export default class extends SfdxCommand {
     }
     let deployOutput = '';
     if (deltaMeta && deltaMeta.length > 0) {
-      deployOutput += `${deltaMeta.join(',').replace()}`;
+      deployOutput += `-p \"${deltaMeta.join(',')}\"`;
     } else {
       deployOutput += `-p ${basedir}`;
     }

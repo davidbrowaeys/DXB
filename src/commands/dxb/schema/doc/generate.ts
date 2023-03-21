15e43e28053151a861d028b11845c640e78bef24
David Browaeys <dbrowaeys@deloitte.com.au>
2023-03-21 16:59:17 +1100

    pdf gen

--- src/commands/dxb/schema/doc/generate.ts
+++ src/commands/dxb/schema/doc/generate.ts
@@ -1,30 +1,12 @@
 import { SfdxCommand } from '@salesforce/command';
-import { Connection } from '@salesforce/core';
+import { Connection, SfdxError } from '@salesforce/core';
 import * as fs from 'fs';
 import * as pdf from 'pdf-creator-node';
 
 const STDOBJECTS = "'Account','Contact','AccountContactRelation','Opportunity','Asset','Event','Task'";
 const STDQUERY = "SELECT Id, DurableId, LastModifiedDate, LastModifiedById, QualifiedApiName, NamespacePrefix, DeveloperName, MasterLabel, Label, PluralLabel, DefaultCompactLayoutId, IsCustomizable, IsApexTriggerable, IsWorkflowEnabled, IsProcessEnabled, IsCompactLayoutable, DeploymentStatus, KeyPrefix, IsCustomSetting, IsDeprecatedAndHidden, IsReplicateable, IsRetrieveable, IsSearchLayoutable, IsSearchable, IsTriggerable, IsIdEnabled, IsEverCreatable, IsEverUpdatable, IsEverDeletable, IsFeedEnabled, IsQueryable, IsMruEnabled, DetailUrl, EditUrl, NewUrl, EditDefinitionUrl, HelpSettingPageName, HelpSettingPageUrl, RunningUserEntityAccessId, PublisherId, IsLayoutable, RecordTypesSupported, InternalSharingModel, ExternalSharingModel, HasSubtypes, IsSubtype, IsAutoActivityCaptureEnabled, IsInterface, ImplementsInterfaces, ImplementedBy, ExtendsInterfaces, ExtendedBy, DefaultImplementation FROM EntityDefinition WHERE QualifiedApiName IN ("+STDOBJECTS+") ORDER BY NamespacePrefix, QualifiedApiName LIMIT 2000";
 const CUSTOMQUERY = "SELECT Id, DurableId, LastModifiedDate, LastModifiedById, QualifiedApiName, NamespacePrefix, DeveloperName, MasterLabel, Label, PluralLabel, DefaultCompactLayoutId, IsCustomizable, IsApexTriggerable, IsWorkflowEnabled, IsProcessEnabled, IsCompactLayoutable, DeploymentStatus, KeyPrefix, IsCustomSetting, IsDeprecatedAndHidden, IsReplicateable, IsRetrieveable, IsSearchLayoutable, IsSearchable, IsTriggerable, IsIdEnabled, IsEverCreatable, IsEverUpdatable, IsEverDeletable, IsFeedEnabled, IsQueryable, IsMruEnabled, DetailUrl, EditUrl, NewUrl, EditDefinitionUrl, HelpSettingPageName, HelpSettingPageUrl, RunningUserEntityAccessId, PublisherId, IsLayoutable, RecordTypesSupported, InternalSharingModel, ExternalSharingModel, HasSubtypes, IsSubtype, IsAutoActivityCaptureEnabled, IsInterface, ImplementsInterfaces, ImplementedBy, ExtendsInterfaces, ExtendedBy, DefaultImplementation FROM EntityDefinition WHERE DeploymentStatus != null AND IsCustomizable = TRUE ORDER BY NamespacePrefix, QualifiedApiName LIMIT 2000";
-
-const PDFOPTION = {
-    format: "A3",
-    orientation: "portrait",
-    border: "10mm",
-    header: {
-        height: "45mm",
-        contents: '<div style="text-align: center;">Author: David Browaeys</div>'
-    },
-    footer: {
-        height: "28mm",
-        contents: {
-            first: 'Cover page',
-            2: 'Second page', // Any page number is working. 1-based index
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-            last: 'Last Page'
-        }
-    }
-};
+const FIELDQUERY = "SELECT Id, DurableId, QualifiedApiName, EntityDefinitionId, NamespacePrefix, DeveloperName, MasterLabel, Label, Length, DataType, ServiceDataTypeId, ValueTypeId, ExtraTypeInfo, IsCalculated, IsHighScaleNumber, IsHtmlFormatted, IsNameField, IsNillable, IsWorkflowFilterable, IsCompactLayoutable, Precision, Scale, IsFieldHistoryTracked, IsIndexed, IsApiFilterable, IsApiSortable, IsListFilterable, IsListSortable, IsApiGroupable, IsListVisible, ControllingFieldDefinitionId, LastModifiedDate, LastModifiedById, PublisherId, RunningUserFieldAccessId, RelationshipName, ReferenceTo, ReferenceTargetField, IsCompound, IsSearchPrefilterable, IsPolymorphicForeignKey, IsAiPredictionField, BusinessOwnerId, BusinessStatus, SecurityClassification, ComplianceGroup, Description FROM FieldDefinition WHERE EntityDefinition.QualifiedApiName = '{{object_name}}' ORDER BY NamespacePrefix, QualifiedApiName";
 
 export default class SchemaDocGen extends SfdxCommand {
 
@@ -49,20 +31,36 @@ export default class SchemaDocGen extends SfdxCommand {
 
     public async run() {
         this.connection = this.org.getConnection();
-        const std_objects = await this.query(STDQUERY);
-        const cust_objects = await this.query(CUSTOMQUERY);
+        let std_objects = await this.query(STDQUERY);
+        await this.getFieldDefinitionForSObject(std_objects);
+        let cust_objects = await this.query(CUSTOMQUERY);
+        await this.getFieldDefinitionForSObject(cust_objects);
+        const documentMeta:any = JSON.parse(fs.readFileSync("/Users/dbrowaeys/workspace/DXB/src/lib/documentinfo.json", "utf8"));
         const html = fs.readFileSync("/Users/dbrowaeys/workspace/DXB/src/lib/schema-template.html", "utf8");
+        const css = fs.readFileSync("/Users/dbrowaeys/workspace/DXB/src/lib/bootstrap.min.css", "utf8");
         const document = {
             html: html,
             data: {
                 std_objects,
-                cust_objects
+                cust_objects,
+                document: documentMeta.documentInfo,
+                style: "<style>"+css+"</style>"
             },
             path: "./sample_pdfoutput.pdf",
             type: "",
           };
-        const pdfResult = await pdf.create(document, PDFOPTION);
+        const pdfResult = await pdf.create(document, documentMeta.pdfOption);
         console.log(pdfResult);
+    }
+
+    private async getFieldDefinitionForSObject(sobjects: any[]): Promise<any[]> {
+        return Promise.all(sobjects.map(async (o) => {
+            o.fields = await this.query(FIELDQUERY.split('{{object_name}}').join(o.QualifiedApiName));
+            return o;
+        })).catch((error) => {
+            console.log(error);
+            throw new SfdxError(error);
+        });
     }
 
     private async query(soql:string){

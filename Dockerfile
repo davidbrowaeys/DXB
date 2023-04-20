d59a04d3210b86b7d0a35c746786570a25da9d94
David Browaeys <dbrowaeys@deloitte.com.au>
2023-04-21 07:42:55 +1000

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,22 @@
 # node > 14.6.0 is required for the SFDX-Git-Delta plugin
-FROM node:lts-alpine
+FROM node:14
 
 WORKDIR /usr/app
 
 #add usefull tools
-RUN apk add --update --no-cache  \
-      git \
-      findutils \
-      bash \
-      unzip \
-      curl \
-      wget \
-      npm \
-      openjdk11 \
-      openssh-client \
-      perl \
-      jq \
-      chromium
+RUN apt-get update
+RUN apt-get install -y jq
 
 # install node packages
-RUN npm install @salesforce/sfdx-scanner --global
-      npm install --global dxb && \
-      npm install --global sfdx-cli --force && \
-      sfdx plugins:link $(npm root -g)/dxb && \
-      sfdx plugins:link $(npm root -g)/@salesforce/sfdx-scanner && \
-      npm install --global vlocity && \
-      export PUPPETEER_SKIP_DOWNLOAD=true && \
-      npm install --global puppeteer --unsafe-perm
+RUN npm install --global dxb && npm install --global sfdx-cli --force && sfdx plugins:link $(npm root -g)/dxb && npm install --global vlocity && export PUPPETEER_SKIP_DOWNLOAD=true && npm install --global puppeteer --unsafe-perm
+# RUN npm install @salesforce/sfdx-scanner --global
+#       npm install --global dxb && \
+#       npm install --global sfdx-cli --force && \
+#       sfdx plugins:link $(npm root -g)/dxb && \
+#       sfdx plugins:link $(npm root -g)/@salesforce/sfdx-scanner && \
+#       npm install --global vlocity && \
+#       export PUPPETEER_SKIP_DOWNLOAD=true && \
+#       npm install --global puppeteer --unsafe-perm
 
 RUN sfdx --version
 RUN sfdx dxb --help
--- src/commands/dxb/junit/check.ts
+++ src/commands/dxb/junit/check.ts
@@ -3,6 +3,14 @@ import { SfdxError } from '@salesforce/core';
 import * as fs from 'fs';
 import * as xml2js from 'xml2js';
 
+export const CONSOLE_COLORS = {
+    blue: '\x1b[34m',
+    green: '\x1b[32m',
+    red: '\x1b[31m',
+    white: '\x1b[37m',
+    yellow: '\x1b[33m',
+};
+
 export default class JUnitReview extends SfdxCommand {
 
     public static description = 'This command check quality of the junit and flag anything slower than defined threshold';
@@ -15,7 +23,8 @@ export default class JUnitReview extends SfdxCommand {
 
     protected static flagsConfig = {
         junitpath: flags.string({ char: 'p', description: 'Path of junit xml file', required:true }),
-        timethreshold: flags.number({ char: 't', description: 'maximum amount of time that a test method should take to execute (in second).', default: 1})
+        timethreshold: flags.number({ char: 't', description: 'maximum amount of time that a test method should take to execute (in second).', default: 1}),
+        flagaserror: flags.boolean({ char: 'e', description: 'if set, the command will update add failure tags to junit file and throw an error', default: false})
     };
     // Comment this out if your command does not require an org username
     protected static requiresUsername = false;s
@@ -28,8 +37,9 @@ export default class JUnitReview extends SfdxCommand {
 
     public async run() {
         const junitpath = this.flags.junitpath;
-        const threshold:number = parseFloat(this.flags.mintime);
-        this.readJunit(junitpath,threshold);
+        const threshold:number = parseFloat(this.flags.timethreshold);
+        const flagaserror = this.flags.flagaserror;
+        this.readJunit(junitpath,threshold,flagaserror);
     }
     /**
      * Parses a JUnit XML file and modifies it to include failure information for slow tests.
@@ -37,7 +47,7 @@ export default class JUnitReview extends SfdxCommand {
      * @param threshold - The time threshold (in milliseconds) to consider a test method "slow".
      * @throws SfdxError - If the JUnit file is not found.
      */
-    private readJunit(junitPath: string, threshold: number): void {
+    private readJunit(junitPath: string, threshold: number, flagaserror: boolean): void {
         if (!fs.existsSync(junitPath)) {
             throw new SfdxError(`JUnit file not found: ${junitPath}`);
         }
@@ -49,40 +59,42 @@ export default class JUnitReview extends SfdxCommand {
                 return;
             }
             
-            let totalTime = 0.0;
             let numSlowTests = 0;
-
+            let slowTests = [];
             result.testsuites.testsuite.forEach((ts: any) => {
                 ts.testcase.forEach((testcase: any) => {
                     const time = parseFloat(testcase.$.time);
-
-                    totalTime += time;
                     
                     if (!testcase.failure && time >= threshold) {
                         numSlowTests++;
-                        if (numSlowTests === 1) console.log(`Some unit test have been idenfitied below our standard ${threshold}s:`);
-                        console.log(`Class.${testcase.$.classname}.${testcase.$.name}: ${time}s (+${time - threshold}s)`);
-                        testcase.failure = {
-                            '_': `Class.${testcase.$.classname}.${testcase.$.name}: line 0, column 0`,
-                            '$': {
-                                'message': 'DXB.PerformanceException: Test method seems a bit slow'
-                            }
-                        };
+                        if (numSlowTests === 1) console.log(`${CONSOLE_COLORS.yellow}%s\x1b[0m`, `Some unit test have been idenfitied below our standard ${threshold}s:`);
+                        slowTests.push({
+                            ClassName: testcase.$.classname,
+                            TestName: testcase.$.name,
+                            time: time, 
+                            Diff: `+${time - threshold}s`
+                        });
+                        if (flagaserror){
+                            testcase.failure = {
+                                '_': `Class.${testcase.$.classname}.${testcase.$.name}: line 0, column 0`,
+                                '$': {
+                                    'message': 'DXB.PerformanceException: Test method seems a bit slow'
+                                }
+                            };
+                        }
                     }
                 });
             });
 
-            const numTests = result.testsuites.testsuite.reduce((total: number, ts: any) => total + ts.testcase.length, 0);
-            const avgTime = totalTime / numTests;
-
-            if (avgTime >= threshold) {
-                console.log(`Overall test classes are experiencing some performance issues. In average we have identified a processing time of ${avgTime}ms per test method which is above our standard threshold of ${threshold}s.`);
+            if (slowTests.length > 0){
+                console.table(slowTests);
             }
 
-            if (numSlowTests > 0) {
+            if (numSlowTests > 0&& flagaserror) {
                 const builder = new xml2js.Builder({ cdata: true });
                 const xml = builder.buildObject(result);
                 fs.writeFileSync(junitPath, xml);
+                throw new SfdxError('DXB.PerformanceException');
             }
         });
     }

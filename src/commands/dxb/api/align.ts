e9949f14db2820ba75c03379f8ad24d2e411d0b0
Olivier Brabanders <obrabanders@deloitte.com>
2023-09-21 16:28:02 +0200

    Cleanup code structure

--- src/commands/dxb/api/align.ts
+++ src/commands/dxb/api/align.ts
@@ -41,35 +41,45 @@ export default class ApiAlign extends SfdxCommand {
       const filesWithApi : string[] = this.findFilesWithTag(packageDir.path, 'apiVersion');
       // for every file with the required tag, read it and update the value of the tag to the project api
       filesWithApi.forEach(( f:string ) => {
-        const fileContent : string = fs.readFileSync(f, { encoding: 'utf-8'});
-        const parser = new xml2js.Parser({ explicitArray : false });
-        parser.parseString(fileContent, function (err, result) {
-          if (err) {
-            console.error(err);
-          } else if (result) {
-            const root = Object.keys(result)[0];
-            if (metadataTypes?.includes(root) === false) { // in case specific metadata types are wanted, skip if this type is not one of them
-              return;
-            }
-            delete result[root]["$"];
-            result[root]["@"] = {
-              xmlns: "http://soap.sforce.com/2006/04/metadata"
-            };
-            console.log(`Change API Version of ${f} from ${result[root].apiVersion} to ${projectApi}`);
-            result[root].apiVersion = projectApi;
-            const xml = js2xmlparser.parse(
-              root,
-              result[root],
-              { 
-                declaration: { encoding: 'UTF-8' },
-                format: {
-                  doubleQuotes: true
-                }
-              });
-            fs.writeFileSync(f, xml);
-          }
-        });
+        this.processFile(f, projectApi, metadataTypes);
       });
+    });
+  }
+
+  /**
+   * Process one file to update the apiVersion tag
+   * @param filePath The path to a file
+   * @param projectApi The API version of the sfdx-project.json
+   * @param metadataTypes The list of Metadata Types that need to be aligned (optional)
+   */
+  private processFile(filePath: string, projectApi : string, metadataTypes? : string[]) {
+    const fileContent: string = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    const parser = new xml2js.Parser({ explicitArray: false });
+    parser.parseString(fileContent, function (err, result) {
+      if (err) {
+        console.error(err);
+      } else if (result) {
+        const root = Object.keys(result)[0];
+        if (metadataTypes?.includes(root) === false) { // in case specific metadata types are wanted, skip if this type is not one of them
+          return;
+        }
+        delete result[root]["$"];
+        result[root]["@"] = {
+          xmlns: "http://soap.sforce.com/2006/04/metadata"
+        };
+        console.log(`Change API Version of ${filePath} from ${result[root].apiVersion} to ${projectApi}`);
+        result[root].apiVersion = projectApi;
+        const xml = js2xmlparser.parse(
+          root,
+          result[root],
+          {
+            declaration: { encoding: 'UTF-8' },
+            format: {
+              doubleQuotes: true
+            }
+          });
+        fs.writeFileSync(filePath, xml);
+      }
     });
   }
 

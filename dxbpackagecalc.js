aec419a5e2ac3c59a6e92c61b156bcf75e16017e
= <=>
2023-10-02 11:52:55 +0200

    update for pipeline

--- dxbpackagecalc.js
+++ dxbpackagecalc.js
@@ -4,24 +4,47 @@ const fs = require("fs");
 fs.readFile("package.json", (err, data) => {
   if (err) throw err;
 
+  const releaseType = process.argv.length >= 2 ? process.argv[2] : undefined;
+
   //Parse the file contents
   let packageJSON = JSON.parse(data);
 
   console.log("DXB Current Version: ", packageJSON.version);
-  //Increment the version attribute
-  let version = packageJSON.version.split(".");
-  if (version[2] == 9) {
-    version[2] = 0;
-    if (version[1] == 9) {
-      version[1] = 0;
-      version[0]++;
+  
+  //if release type is beta then increment and add beta other
+  let fullversion = packageJSON.version.split("-");
+  let isbeta = fullversion[1];
+  let version = fullversion[0].split(".");
+  if (releaseType === 'beta'){
+    if (version[2] == 9) {
+      version[2] = 0;
+      if (version[1] == 9) {
+        version[1] = 0;
+        version[0]++;
+      } else {
+        version[1]++;
+      }
     } else {
-      version[1]++;
+      version[2]++;
     }
-  } else {
-    version[2]++;
+    packageJSON.version = version.join(".");
+    packageJSON.version += '-beta';
+  }else if (isbeta === 'beta'){
+      packageJSON.version = fullversion[0];
+  }else{
+      if (version[2] == 9) {
+        version[2] = 0;
+        if (version[1] == 9) {
+          version[1] = 0;
+          version[0]++;
+        } else {
+          version[1]++;
+        }
+      } else {
+        version[2]++;
+      }
+      packageJSON.version = version.join(".");
   }
-  packageJSON.version = version.join(".");
   console.log("DXB New Version: ", packageJSON.version);
 
   //Write the updated package.json file

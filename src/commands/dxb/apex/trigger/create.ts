401e930a04b767d09246dd970f51bb414922ccc9
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-02 15:29:35 +0200

    Migrate apex:trigger:create

--- src/commands/dxb/apex/trigger/create.ts
+++ src/commands/dxb/apex/trigger/create.ts
@@ -1,118 +1,124 @@
-import { flags, SfdxCommand } from '@salesforce/command';
-import { SfdxProject } from '@salesforce/core';
+import path = require('path');
+import fse = require('fs-extra');
 
-const path = require('path');
-const fse = require('fs-extra');
-const fs = require('fs');
- export default class ApexTriggerCreation extends SfdxCommand {
-     
-    public static description = 'This command create trigger and apex class by simply specifying object name according to domain layer.';
+import {Flags, SfCommand} from '@salesforce/sf-plugins-core';
+import { Messages, SfProject } from '@salesforce/core';
 
-    public static examples = [
-        `$ sfdx dxb:apex:trigger:create -o Account`
-    ];
+type ApexTriggerCreationResult = {
+  success: boolean;
+}
 
-    public static args = [{ name: 'file' }];
 
-    protected static flagsConfig = {
-        sobject: flags.string({
-            char: 'o',
-            required: true,
-            description: 'api name of SObject'
-        }),
-        sourceapiversion: flags.number({
-            char: 'v',
-            description: 'set api version of the generated class'
-        })
-    };
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('dxb', 'apex.trigger.create');
 
-    public async run() : Promise<any> {
-        const project = await SfdxProject.resolve();
-        var config: any = await project.resolveProjectConfig();
-        const sobject = this.flags.sobject;
-        const apiversion = this.flags.sourceApiVersion | config.sourceApiVersion;
+export default class ApexTriggerCreation extends SfCommand<ApexTriggerCreationResult> {
 
-        const template = 'trigger';
-        const vars =    'className=' + sobject.replace('__c', '').replace('_', '') + 'TriggerHandler,' +
-                        'triggerName=' + sobject.replace('__c', '').replace('_', '') + 'Trigger,' +
-                        'apiVersion=' + apiversion.toPrecision(3); + ',' +
-                        'sobject=' + sobject;
-        
-        let templateFolder = path.join('.sfdx-templates', template);
-        if (!fse.existsSync(templateFolder)) {
-            templateFolder = path.join(__dirname, '../../../../utils/templates/', template);
-        }
-        console.log(templateFolder);
-    
-        createFiles(templateFolder, sobject, vars, null);
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+
+  public static readonly examples = messages.getMessages('examples');
+
+  public static readonly flags = {
+    sobject: Flags.string({
+      char: 's',
+      required: true,
+      summary: messages.getMessage('flags.sobject.summary')
+    }),
+    'source-api-version': Flags.integer({
+      char: 'v',
+      summary: messages.getMessage('flags.source-api-version.summary')
+    })
+  };
+
+  public async run(): Promise<ApexTriggerCreationResult> {
+    const {flags} = await this.parse(ApexTriggerCreation);
+    const project = await SfProject.resolve();
+    const config: any = await project.resolveProjectConfig();
+    const sobject = flags.sobject;
+    const apiversion: number = flags['source-api-version'] ?? config.sourceApiVersion;
+
+    const template = 'trigger';
+    const vars =    'className=' + sobject.replace('__c', '').replace('_', '') + 'TriggerHandler,' +
+    'triggerName=' + sobject.replace('__c', '').replace('_', '') + 'Trigger,' +
+    'apiVersion=' + apiversion.toPrecision(3) + ',' +
+    'sobject=' + sobject;
+
+    let templateFolder = path.join('.sfdx-templates', template);
+    if (!fse.existsSync(templateFolder)) {
+      templateFolder = path.join(__dirname, '../../../../utils/templates/', template);
     }
+    this.log(templateFolder);
 
-}
+    this.createFiles(templateFolder, sobject, vars);
+    return { success: true };
+  }
 
-function updateContent(content, values) {
-    const splitValues = values.split('=');
-
-    const varName = splitValues[0];
-    const varValue = splitValues[1];
-    content = content.replace(new RegExp(`{{${varName}}}`, 'g'), varValue);
-    return content;
-}
-
-function createFiles(templateFolder, sobject, vars, done) {
+  private createFiles(templateFolder: string, sobject: string, vars: string): void {
 
     const name = sobject.replace('__c', '').replace('_', '') + 'Trigger';
     const outputdir = './force-app/main/default';
 
     if (!fse.existsSync(templateFolder)) {
-        done(`specified template 'trigger' doesn't exist`, null);
+      this.error(messages.getMessage('error.templateNotExist'));
     }
 
     const defJsonPath = path.join(templateFolder, 'def.json');
 
     if (!fse.existsSync(defJsonPath)) {
-        done('def.json not found', null);
+      this.error(messages.getMessage('error.defJsonNotFound'));
     }
 
-    const defJson = JSON.parse(fs.readFileSync(defJsonPath).toString());
+    const defJson: { files: string[][]; vars: string } = JSON.parse(fse.readFileSync(defJsonPath).toString());
     const defJsonVars = defJson.vars;
 
     if (!vars) {
-        done(`The following variables are required: ${defJsonVars}. Specify them like: -v className=myclass,apiName=40.0`, null);
+      this.error(messages.getMessage('error.defJSONVars', [defJsonVars]));
     }
 
-    const filesCreated = [];
+    const filesCreated: string[] = [];
 
     defJson.files.forEach((row) => {
-        const fileName = row[0];
-        const fileExtension = row[1];
-        if (fileName !== 'def.json') {
+      const fileName = row[0];
+      const fileExtension = row[1];
+      if (fileName !== 'def.json') {
 
-            const templateFilePath = path.join(templateFolder, fileName);
-            let content = fs.readFileSync(templateFilePath).toString();
+        const templateFilePath = path.join(templateFolder, fileName);
+        let content = fse.readFileSync(templateFilePath).toString();
 
-            const splitVars = vars.split(',');
-            splitVars.forEach((value) => {
-                content = updateContent(content, value);
-            });
-            content = updateContent(content, 'sobject='+sobject);
-            
-            let newFile = path.join(`${outputdir}/triggers`, `${name}.${fileExtension}`);
-            if (fileExtension.toString().includes('cls')) {
-                newFile = path.join(`${outputdir}/classes`, `${name}Handler.${fileExtension}`);
-            }
+        const splitVars = vars.split(',');
+        splitVars.forEach((value) => {
+          content = updateContent(content, value);
+        });
+        content = updateContent(content, 'sobject='+sobject);
 
-            const newFilePath = path.dirname(newFile);
-
-            fse.ensureDirSync(newFilePath);
-            fs.writeFileSync(newFile, content);
-            filesCreated.push(newFile);
+        let newFile = path.join(`${outputdir}/triggers`, `${name}.${fileExtension}`);
+        if (fileExtension.toString().includes('cls')) {
+          newFile = path.join(`${outputdir}/classes`, `${name}Handler.${fileExtension}`);
         }
+
+        const newFilePath = path.dirname(newFile);
+
+        fse.ensureDirSync(newFilePath);
+        fse.writeFileSync(newFile, content);
+        filesCreated.push(newFile);
+      }
     });
 
     let result = 'The following files were created:';
-    for (let i = 0; i < filesCreated.length; i++) {
-        result += `\n  ${filesCreated[i]}`;
+    for (const createdFile of filesCreated) {
+      result += `\n  ${createdFile}`
     }
 
-    console.log(result);
+    this.log(result);
+  }
+}
+
+function updateContent(content: string, values: string): string {
+  const splitValues = values.split('=');
+
+  const varName = splitValues[0];
+  const varValue = splitValues[1];
+  content = content.replace(new RegExp(`{{${varName}}}`, 'g'), varValue);
+  return content;
 }

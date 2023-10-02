1afda890d9f4a649b4917e5da27b2c34e9308deb
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-02 15:27:16 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
-/* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ['eslint-config-salesforce-typescript', 'plugin:sf-plugin/migration'],
+  parserOptions: {
+    tsconfigRootDir: __dirname
+  },
   root: true,
+  rules: {
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+  }
 };

fb78a878d651dcc85b27a64b15f50543ee389d2c
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-03 10:08:14 +0200

    Migrate community:publish

--- src/commands/dxb/community/publish.ts
+++ src/commands/dxb/community/publish.ts
@@ -1,44 +1,42 @@
-import { flags, SfdxCommand } from '@salesforce/command';
-const exec = require('child_process').execSync;
+import { execSync as exec } from 'child_process';
+import {Flags, SfCommand } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
 
-export default class CommunityPublish extends SfdxCommand {
+type CommunityPublishResult = {
+  success: boolean;
+}
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('dxb', 'community.publish');
+export default class CommunityPublish extends SfCommand<CommunityPublishResult> {
 
-    public static description = 'Publish experience community to target environment. If not specified, then will fetch all "Live" communities from target env.';
+  public static readonly summary = messages.getMessage('summary');
 
-    public static examples = [
-        `$ sfdx dxb:community:publish`,
-        `$ sfdx dxb:community:publish -n portal1,partner1`
-    ];
+  public static readonly examples = messages.getMessages('examples');
 
-    public static args = [{ name: 'file' }];
+  public static readonly flags = {
+    'target-org': Flags.requiredOrg(),
+    name: Flags.string({char:'n',summary:messages.getMessage('flags.name.summary'), multiple: true})
+  };
 
-    protected static flagsConfig = {
-        name: flags.string({char:'n',description:'List of community name separated by comma. If not specified, then will fetch all "Live" communities from target env'})
-    };
-    // Comment this out if your command does not require an org username
-    protected static requiresUsername = true;
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  public static readonly requiresProject = true;
 
-    // Comment this out if your command does not support a hub org username
-    protected static supportsDevhubUsername = false;
-
-    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-    protected static requiresProject = true;
-
-    public async run() {
-        let name = this.flags.name;
-        const username = this.org.getUsername();
-        if (name){
-            name.split(',').forEach( (elem) => {
-                console.log(exec(`sfdx force:community:publish -n ${elem} -u ${username}`).toString());
-            })
-        }else{
-            const allcommunities = JSON.parse(exec(`sfdx force:data:soql:query -q "SELECT Name FROM Network WHERE Status = 'Live'" --resultformat json -u ${username}`).toString());
-            if (allcommunities !== null){
-                allcommunities.result?.records?.forEach( (elem) => {
-                    console.log(exec(`sfdx force:community:publish -n ${elem.Name} -u ${username}`).toString());
-                })
-            }
-        }
-        
+  public async run(): Promise<CommunityPublishResult> {
+    const {flags} = await this.parse(CommunityPublish);
+    const name = flags.name;
+    const username = flags['target-org']?.getUsername();
+    if (name){
+      name.forEach( (elem) => {
+        this.log(exec(`sf community publish --name ${elem} --target-org ${username}`).toString());
+      })
+    } else {
+      const allcommunities = JSON.parse(exec(`sf data query --query "SELECT Name FROM Network WHERE Status = 'Live'" --result-format json --target-org ${username}`).toString());
+      if (allcommunities !== null){
+        allcommunities.result?.records?.forEach( (elem: {Name: string}) => {
+          this.log(exec(`sf community publish --name ${elem.Name} --target-org ${username}`).toString());
+        })
+      }
     }
+    return { success: true };
+  }
 }

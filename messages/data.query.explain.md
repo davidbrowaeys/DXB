ba0668969feebe02b3a4730f42ddf93422f9d2e5
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-03 13:18:51 +0200

    Migrate data:query:explain

---
+++ messages/data.query.explain.md
@@ -0,0 +1,42 @@
+# summary
+
+CLI version of the salesforce query plan tool to optimize and speed up queries.
+
+# examples
+
+- Use a specific query
+  
+  <%= config.bin %> <%= command.id %> --query "select id from Account where BillingCountry = \'Australia\' limit 10"
+
+- Query against a specific org
+  
+  <%= config.bin %> <%= command.id %> -q "select id from Account where BillingCountry = \'Australia\' limit 10" --target-org myOrg
+
+# flags.query.summary
+
+A valid SOQL query
+
+# log.successful
+
+All files downloaded successfully.
+
+# log.connecting
+
+Connecting to org...
+
+# log.noExplanation
+
+No Query explanation available
+
+# log.connected
+
+Connected to %s...
+Access Token: %s
+
+# error.queryNotValid
+
+Connection not valid.
+
+# error.unexpected
+
+Unexpected issue occurred
--- src/commands/dxb/data/query/explain.ts
+++ src/commands/dxb/data/query/explain.ts
@@ -1,114 +1,113 @@
-import { flags, SfdxCommand } from '@salesforce/command';
-import { SfdxError, SfdxProject } from '@salesforce/core';
+import {Flags, SfCommand } from '@salesforce/sf-plugins-core';
+import { Messages, SfError } from '@salesforce/core';
+import { QueryExplainResult } from 'jsforce';
+import * as Table from 'cli-table3';
 
-export default class QueryExplain extends SfdxCommand {
+function displayAsTable(body: QueryExplainResult): string {
+  const table = new Table({
+    head: ['Cardinality', 'Fields','Leading \nOperation Type', 'Relative Cost', 'Object Cardinality','Object Type'],
+    colWidths: [20, 50, 20, 20, 20, 20]
+  });
+  const noteTable = new Table({
+    head: ['Description', 'Fields','TableEnumOrId'],
+    colWidths: [70, 30, 30]
+  });
+  for (const plan of body.plans) {
+    table.push([
+      plan.cardinality,
+      plan.fields.toString(),
+      plan.leadingOperationType,
+      plan.relativeCost,
+      plan.sobjectCardinality,
+      plan.sobjectType
+    ]);
 
-    public static description = 'CLI version of the salesforce query plan tool to optimize and speed up queries.';
-  
-    public static examples = [
-        `$ sfdx dxb:data:quer:explain -u myorg -q "select id from Account where BillingCountry = 'Australia' limit 10"`
-    ];
-  
-    public static args = [{name: 'file'}];
-  
-    protected static flagsConfig = {
-        query: flags.string({char:'q',description:'query'})
+    for (const note of plan.notes) {
+      noteTable.push([
+        note.description,
+        note.fields.toString(),
+        note.tableEnumOrId
+      ]);
+    }
+  }
+  return `${table.toString()}\n=== Notes\n${noteTable.toString()}`;
+}
+
+type Result = {
+  result: any;
+}
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('dxb', 'data.query.explain');
+export default class QueryExplain extends SfCommand<Result> {
+
+  public static readonly summary = messages.getMessage('summary');
+
+  public static readonly examples = messages.getMessages('examples');
+
+  public static readonly flags = {
+    'target-org': Flags.requiredOrg(),
+    query: Flags.string({
+      char:'q',
+      summary:messages.getMessage('flags.query.summary')
+    })
+  };
+
+  protected isJsonOnly: boolean | undefined;
+
+  public async run(): Promise<Result> {
+    const {flags} = await this.parse(QueryExplain);
+    const query = flags.query;
+    this.isJsonOnly = flags.json;
+
+    if (!query){
+      throw new SfError('Must specify query in order to use this command.');
+    }
+
+    if (!this.isJsonOnly) {
+      this.log(messages.getMessage('log.connecting'));
+    }
+    const accessToken = flags['target-org']!.getConnection().accessToken;
+    const instanceUrl = flags['target-org']!.getConnection().instanceUrl;
+
+    if (!accessToken || !instanceUrl){
+      throw messages.createError('error.queryNotValid');
+    }
+
+    if (!this.isJsonOnly){
+      this.log(messages.getMessage('log.connected',[instanceUrl,accessToken]));
+    }
+
+    const result = await this.queryPlan(query, accessToken, instanceUrl);
+    return { result };
+  }
+
+  public async queryPlan(query: string, accessToken: string, instanceUrl: string): Promise<any> {
+    const url = `${instanceUrl}/services/data/v57.0/query/?explain=${encodeURIComponent(query)}`;
+    const headers = new Headers();
+    headers.append('Authorization',  'Bearer ' + accessToken);
+    headers.append('X-SFDC-Session',  'Bearer ' + accessToken);
+    headers.append('Content-Type', 'application/json; charset=UTF-8');
+    headers.append('Accept', 'application/json');
+    const options: RequestInit = {
+      method: 'GET',
+      headers,
     };
-    // Comment this out if your command does not require an org username
-    protected static requiresUsername = true;
-  
-    // Comment this out if your command does not support a hub org username
-    protected static supportsDevhubUsername = false;
-  
-    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-    protected static requiresProject = false;
-  
-    protected isJsonOnly:boolean;
-    protected projectConfig;
-
-    public async run() {
-        let query = this.flags.query;
-        this.isJsonOnly = this.flags.json;
-
-        //project config
-        this.projectConfig = await  (await SfdxProject.resolve()).resolveProjectConfig();
-        if (!query){
-          throw new SfdxError('Must specify query in order to use this command.');
-        }
-
-			  if (!this.isJsonOnly)console.log('Connecting to org...');
-        let accessToken = this.org.getConnection().accessToken;
-        let instanceUrl = this.org.getConnection().instanceUrl;
-        
-        if (!accessToken || !instanceUrl){
-          throw new SfdxError(`Connection not valid.`);
-        }
-
-        if (!this.isJsonOnly)console.log('Connected to ',instanceUrl,'...\n',accessToken);
-
-        const result = await this.queryPlan(query, accessToken, instanceUrl);
-        if (this.isJsonOnly)return result;
-    }
-
-    public async queryPlan(query, accessToken, instanceUrl){
-        const url = `${instanceUrl}/services/data/v57.0/query/?explain=${encodeURIComponent(query)}`;
-        const headers = new Headers();
-        headers.append('Authorization',  'Bearer ' + accessToken);
-        headers.append('X-SFDC-Session',  'Bearer ' + accessToken);
-        headers.append('Content-Type', 'application/json; charset=UTF-8');
-        headers.append('Accept', 'application/json');
-        const options: RequestInit = {
-            method: 'GET',
-            headers: headers,
-        };
-        try{
-        const response = await fetch(url, options);
-        const body:any = await response.json();
-        if (!body.plans || body.plans.length === 0){
-            console.log(body);
-            return;
-        }
-        if (this.isJsonOnly){
-          return body;
-        }
-        this.displayAsTable(body);
+    try{
+      const response = await fetch(url, options);
+      const body: QueryExplainResult = await response.json();
+      if (!body.plans || body.plans.length === 0){
+        this.log(messages.getMessage('log.noExplanation'));
         return;
-      }catch(error){
-        console.error(error);
-        return undefined;
       }
-    }
+      if (this.isJsonOnly){
+        return body;
+      }
 
-    public displayAsTable(body){
-      var Table = require('cli-table');
-        var table = new Table({
-              head: ['Cardinality', 'Fields','Leading \nOperation Type', 'Relative Cost', 'Object Cardinality','Object Type'], 
-              colWidths: [20, 50, 20, 20, 20, 20]
-        });
-        var noteTable = new Table({
-              head: ['Description', 'Fields','TableEnumOrId'], 
-              colWidths: [70, 30, 30]
-        });
-        for (var i = 0 ; i < body.plans.length; i++){
-          table.push([
-              body.plans[i].cardinality,
-              body.plans[i].fields.toString(),
-              body.plans[i].leadingOperationType,
-              body.plans[i].relativeCost,
-              body.plans[i].sobjectCardinality,
-              body.plans[i].sobjectType
-            ]);
-
-          for (var n = 0; n < body.plans[i].notes.length; n++){
-            noteTable.push([
-                body.plans[i].notes[n].description,
-                body.plans[i].notes[n].fields.toString(),
-                body.plans[i].notes[n].tableEnumOrId
-            ]); 
-          }
-        }
-        console.log(table.toString());
-        console.log('=== Notes');
-        console.log(noteTable.toString());
+      return displayAsTable(body);;
+    }catch(error){
+      const e = error as Error;
+      throw messages.createError('error.unexpected', undefined, undefined, e, e);
     }
+  }
 }

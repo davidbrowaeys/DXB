9ce53e0059fc4e2c1681fc156b884218d0a93ebb
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-03 13:20:16 +0200

    Disable eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off'
   }
 };

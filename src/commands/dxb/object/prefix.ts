a29b87cf097fb7f56b323a23d777c269d4ce629d
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-09 10:10:06 +0200

    Migrate object:prefix

--- src/commands/dxb/object/prefix.ts
+++ src/commands/dxb/object/prefix.ts
@@ -1,113 +1,73 @@
+import {Flags, SfCommand } from '@salesforce/sf-plugins-core';
+import { Connection, Messages } from '@salesforce/core';
+import { DescribeSObjectResult } from 'jsforce';
+import { DescribeGlobalSObjectResult } from 'jsforce/lib/api/soap/schema';
 
-import { flags, SfdxCommand } from '@salesforce/command';
-import { SfError } from '@salesforce/core';
-
-const exec = require('child_process').execSync;
-const request = require('request');
-
-function retrievesobjectfields(orgname, sobject){
-    console.log(`Retrieving ${sobject} schema...`); 
-    orgname = orgname ? ('-u '+ orgname) : '';
-    return exec(`sfdx force:schema:sobject:describe -s ${sobject} ${orgname} --json`).toString();
+type SObjectPrefixResult = {
+  result: string;
 }
 
-async function retrievesglobalschema(accessToken, instanceUrl){
-    console.log(`Retrieving global schema...`); 
-    try{
-        return new Promise(async function (resolve, reject) {
-            const options = {
-                method  : 'GET',
-                headers : {
-                            'Authorization' : 'Bearer ' + accessToken,
-                            'X-SFDC-Session' : accessToken
-                            },
-                url     : instanceUrl+'/services/data/v45.0/sobjects/',
-                json: true,
-            };
-            await request(options, function (error, response, body) {
-                if (error || response.statusCode !== 200){
-                    throw new SfError(error ? error : response.statusMessage);
-                }
-                resolve(body.sobjects);
-            });
-        });
-    }catch(err){
-        throw new SfError(`Unable to access ${instanceUrl}`);
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('dxb', 'object.prefix');
+export default class SObjectPrefix extends SfCommand<SObjectPrefixResult> {
+  
+  public static readonly summary = messages.getMessage('summary');
+  
+  public static readonly examples = messages.getMessages('examples');
+  
+  public static readonly flags = {
+    'target-org': Flags.requiredOrg(),
+    'object-name': Flags.string({char:'s',summary: messages.getMessage('flags.object-name.summary'), exclusive: ['prefix']}),
+    prefix: Flags.string({char:'p', summary: messages.getMessage('flags.prefix.summary'), exclusive: ['object-name']})
+  };
+  
+  protected connection: Connection | undefined;
+  
+  public async run(): Promise<SObjectPrefixResult> {
+    const {flags} = await this.parse(SObjectPrefix);
+    this.connection = flags['target-org']!.getConnection()!;
+    const sobject = flags.objectname;
+    const prefix = flags.prefix;
+    if (!sobject && !prefix){
+      throw messages.createError('error.invalidArguments');
     }
-}
-
-export default class SObjectPrefix extends SfdxCommand {
-
-    public static description = 'Retrieve key prefix of specified sobject.';
-  
-    public static examples = [
-    `$ sfdx dxb:object:prefix -s Account
-    Retrieving Account schema...
-    ==== Object Prefix:      001
-    `,
-    `$ sfdx dxb:object:prefix -p 001
-    Retrieving global schema...
-    ==== Object Name:      Account
-    `,
-    ];
-  
-    public static args = [{name: 'file'}];
-  
-    protected static flagsConfig = {
-        objectname: flags.string({char:'s',description:'API Name of custom object'}),
-        prefix: flags.string({char:'p', description: 'prefix of the object'})
-    };
-    // Comment this out if your command does not require an org username
-    protected static requiresUsername = true;
-  
-    // Comment this out if your command does not support a hub org username
-    protected static supportsDevhubUsername = false;
-  
-    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-    protected static requiresProject = false;
-  
-    public async run() {
-        let sobject = this.flags.objectname;
-        let prefix = this.flags.prefix;
-        if (!sobject && !prefix){
-            throw new SfError('You must specify either objectname or prefix.');
+    if (sobject){
+      const orgname = flags['target-org']!.getUsername()!;
+      const keyPrefix = (await this.retrievesobjectfields(orgname,sobject)).keyPrefix;
+      return {result: messages.getMessage('log.result.prefix', [keyPrefix])};
+    } else if (prefix){
+      const accessToken = flags['target-org']?.getConnection()?.accessToken;
+      const instanceUrl = flags['target-org']?.getConnection()?.instanceUrl;
+      
+      if (!accessToken || !instanceUrl){
+        throw messages.createError('error.invalidConnection');
+      }
+      
+      let objectName;
+      const globalschema: DescribeGlobalSObjectResult[] = await this.retrieveGlobalSchema();
+      for (const o of globalschema) {
+        if (o.keyPrefix === prefix) {
+          objectName = o.name;
+          break;
         }
-
-        if (sobject && prefix){
-            throw new SfError('You can only specify either objectname or prefix.');
-        }
-        try{
-            if (sobject){
-                let orgname = this.org.getUsername();
-                var objectschema = retrievesobjectfields(orgname,sobject);
-                objectschema = JSON.parse(objectschema).result.keyPrefix;
-                this.ux.log('==== Object Prefix:    ',objectschema);
-            } else if (prefix){
-                let accessToken = this.org.getConnection().accessToken;
-                let instanceUrl = this.org.getConnection().instanceUrl;
-
-                if (!accessToken || !instanceUrl){
-                    throw new SfError(`Connection not valid.`);
-                }
-
-                var objectName;
-                var globalschema:any = await retrievesglobalschema(accessToken,instanceUrl);
-                
-                for ( var i in globalschema){
-                    var elem = globalschema[i];
-                    if (elem.keyPrefix === prefix){
-                        objectName = elem.name;
-                        break;
-                    }
-                }
-                if (objectName){
-                    this.ux.log('==== Object Name:    ',objectName);
-                }else{
-                    this.ux.error('Prefix not found.');
-                }
-            }
-        }catch(err){
-            this.ux.error(err);
-        }
+      }
+      if (objectName){
+        return { result: messages.getMessage('log.result.objectname',[objectName])};
+      } else {
+        throw messages.createError('error.prefixNotFound');
+      }
+    } else {
+      throw messages.createError('error.unexpected');
     }
+  }
+  
+  private async retrievesobjectfields(orgname: string, sobject: string): Promise<DescribeSObjectResult>{
+    this.log(messages.getMessage('log.getFields', [sobject, orgname]));
+    return this.connection!.describeSObject(sobject);
+  }
+  
+  private async retrieveGlobalSchema(): Promise<DescribeGlobalSObjectResult[]>{
+    this.log(messages.getMessage('log.globalSchema'));
+    return (await this.connection!.describeGlobal()).sobjects;
+  }
 }

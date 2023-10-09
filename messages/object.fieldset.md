4e843879f8fec13a906dc98fd422aa9a86242bd6
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-09 08:22:25 +0200

    Migrate object:fieldset

---
+++ messages/object.fieldset.md
@@ -0,0 +1,76 @@
+# summary
+
+Create a new fieldset for an object. Push to org if needed.
+
+# examples
+
+- Specify the SObject to create
+ 
+ <%= config.bin %> <%= command.id %> --object-name Account --fieldset-name FS_1
+
+- Choose if you want to push the new Object to the connected org
+ 
+ <%= config.bin %> <%= command.id %> -s Account -n FS_1 --push
+
+- Specify a target org to deploy to
+
+ <%= config.bin %> <%= command.id %> -s Account -n FS_1 -p --target-org myOrg
+
+- Display all fields on the Object before creating the field set
+
+ <%= config.bin %> <%= command.id %> -s Account -n FS_1 -p --retrieve-fields
+
+# flags.object-name.summary
+
+Name of custom object
+
+# flags.fieldset-name.summary
+
+Name of fieldset
+
+# flags.retrieve-fields.summary
+
+Retrieve and display sobject fields in terminal
+
+# flags.push.summary
+
+Push the changes to the target org
+
+# log.fieldsetCreated
+
+=== Fieldset created successfully: %s
+
+# log.availableFields
+
+=== Available Object Fields:
+%s
+
+# log.getFields
+
+Retrieve schema for %s from %s
+
+# error.pushFailed
+
+Unable to push source to org!
+
+# error.exists
+
+This object already exists
+
+# data.fieldset
+
+<?xml version="1.0" encoding="UTF-8"?>
+<fieldSets xmlns="http://soap.sforce.com/2006/04/metadata">
+  <fullName>{{fullname}}</fullName>
+  <description>{{description}}</description>
+  {{fieldlist}}
+  <label>{{label}}</label>
+</fieldSets>
+
+# prompt.message.fields
+
+Fields (APIName with comma separated): 
+
+# prompt.message.description
+
+Description: 
--- src/commands/dxb/object/fieldset.ts
+++ src/commands/dxb/object/fieldset.ts
@@ -1,124 +1,109 @@
+import { execSync as exec } from 'child_process';
+import {Flags, SfCommand } from '@salesforce/sf-plugins-core';
+import { Connection, Messages } from '@salesforce/core';
+import * as fs from 'fs-extra';
+import { DescribeSObjectResult } from 'jsforce';
 
-import { flags, SfdxCommand } from '@salesforce/command';
-import { SfError } from '@salesforce/core';
-
-const fs = require('fs');
-const exec = require('child_process').exec;
-
-var fieldlist = '';
-var content =   '<?xml version="1.0" encoding="UTF-8"?>\n'+
-                '<fieldSets xmlns="http://soap.sforce.com/2006/04/metadata">\n'+
-                '   <fullName>{{fullname}}</fullName>\n'+
-                '   <description>{{description}}</description>\n'+
-                '{{fieldlist}}'+
-                '   <label>{{label}}</label>\n'+
-                '</fieldSets>';
-
-function updateContent(varName, question) {
-    var stdin = require('readline-sync');
-    var res = stdin.question(question);
-    
-    content = content.replace(new RegExp(`{{${varName}}}`, 'g'), res);
+type FieldSetCreateResult = {
+  output: string;
 }
 
-async function push_source(orgname){
-    console.log('Push source to org...'); 
-    try{
-      return new Promise(async function (resolve, reject) {
-          await exec(`sfdx force:source:push -g -f -u ${orgname}`, (error, stdout, stderr) => {
-            resolve(stdout? stdout : stderr);
-          });
-      });
-    }catch(err){
-      throw new SfError('Unable to push source to scratch org!');
-    }
-}
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('dxb', 'object.fieldset');
+export default class FieldSetCreate extends SfCommand<FieldSetCreateResult> {
 
-async function retrievesobjectfields(orgname, sobject){
-    console.log('Push source to org...'); 
-    try{
-      return new Promise(async function (resolve, reject) {
-          await exec('sfdx force:schema:sobject:describe -s '+sobject+' '+(orgname ? '-u '+ orgname : '') +' --json', (error, stdout, stderr) => {
-            if (error){
-                throw new SfError('Unable to retrieve sobject fields from scratch org!');
-            }
-            resolve(stdout? stdout : stderr);
-          });
-      });
-    }catch(err){
-      throw new SfError('Unable to retrieve sobject fields from scratch org!');
-    }
-}
+  public static readonly summary = messages.getMessage('summary');
 
-export default class FieldSetCreate extends SfdxCommand {
+  public static readonly examples = messages.getMessages('examples');
 
-    public static description = 'Create fieldset for specified object and push to scratch org.';
-  
-    public static examples = [
-    `$ sfdx dxb:object:create --targetusername myOrg@example.com --objectname Invoice`
-    ];
-  
-    public static args = [{name: 'file'}];
-  
-    protected static flagsConfig = {
-        objectname: flags.string({char:'o',description:'Name of custom object',required:true}),
-        fieldsetname: flags.string({char: 'n',description: 'Fieldset name',required:true}),
-        retrievefields: flags.boolean({char: 'f',description: 'retrieve and display sobject fields in terminal'})
-    };
-    // Comment this out if your command does not require an org username
-    protected static requiresUsername = true;
-  
-    // Comment this out if your command does not support a hub org username
-    protected static supportsDevhubUsername = true;
-  
-    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-    protected static requiresProject = false;
-  
-    public async run() {
-        let orgname = this.org.getUsername();
-        let sobject = this.flags.objectname;
-        let name = this.flags.fieldsetname;
+  public static readonly flags = {
+    'target-org': Flags.requiredOrg(),
+    'object-name': Flags.string({char:'s',summary: messages.getMessage('flags.object-name.summary'),required:true}),
+    'fieldset-name': Flags.string({char: 'n',summary: messages.getMessage('flags.fieldset-name.summary'),required:true}),
+    'retrieve-fields': Flags.boolean({char: 'f',summary: messages.getMessage('flags.retrieve-fields.summary')}),
+    push: Flags.boolean({char: 'p', summary: messages.getMessage('flags.push.summary'), default: false})
+  };
 
-        var vrpath = `./force-app/main/default/objects/${sobject}/fieldSets`;
-        if (!fs.existsSync(vrpath)) {
-            fs.mkdirSync(vrpath);
-        }
+  protected connection: Connection | undefined;
 
-        let apiname = name.replace(new RegExp(`[^A-Z0-9]`,'gi'), '_');
-        content = content.replace(new RegExp(`{{label}}`, 'g'), name);
-        content = content.replace(new RegExp(`{{fullname}}`, 'g'), apiname);
+  private content: string = messages.getMessage('data.fieldset');
 
-        updateContent('description','Description: ');
+  public async run(): Promise<FieldSetCreateResult> {
+    const {flags} = await this.parse(FieldSetCreate);
+    const orgname = flags['target-org']!.getUsername()!;
+    this.connection = flags['target-org']?.getConnection();
+    const sobject = flags.objectname;
+    const name = flags.fieldsetname;
 
-        if (this.flags.retrievefields){
-            var objectschema:any = retrievesobjectfields(orgname,sobject).toString();
-            objectschema = JSON.parse(objectschema);
+    const fsPath = `./force-app/main/default/objects/${sobject}/fieldSets`;
+    fs.ensureDirSync(fsPath);
 
-            if (objectschema.result.queryable){
-                var fields = "Name";
-                objectschema.result.fields.forEach(function(f){
-                    if(!f.deprecatedAndHidden && f.name !== 'Name') fields = fields +","+ f.name;
-                });
-            }
-            console.log('\n=== Available Object Fields \n'+fields+'\n');
-        }
-        var stdin = require('readline-sync');
-        var res = stdin.question('Fields (APIName with comma separated): ');
-        res.split(',').forEach(function(elem){
-            var displayFieldTemplate = 
-            '   <displayedFields>\n'+
-            '       <field>{{fieldname}}</field>\n'+
-            '   </displayedFields>\n';
-            fieldlist += displayFieldTemplate.replace(new RegExp(`{{fieldname}}`, 'g'), elem.trim());
+    const apiname = name.replace(new RegExp('[^A-Z0-9]','gi'), '_');
+    this.content = this.content.replace(new RegExp('{{label}}', 'g'), name);
+    this.content = this.content.replace(new RegExp('{{fullname}}', 'g'), apiname);
+
+    await this.updateContent('description', messages.getMessage('prompt.message.description'));
+
+    if (flags.retrievefields){
+      const objectschema = await this.retrievesobjectfields(orgname,sobject);
+
+      if (objectschema.queryable){
+        let fields = 'Name';
+        objectschema.fields.forEach((f) =>{
+          if(!f.deprecatedAndHidden && f.name !== 'Name') {
+            fields = fields +','+ f.name;
+          }
         });
-        content = content.replace(new RegExp(`{{fieldlist}}`, 'g'), fieldlist);
-
-        //update content file
-        const fullpath = vrpath+'/'+apiname+'.fieldSet-meta.xml';
-        fs.writeFileSync(fullpath, content);
-        console.log(`\n=== Fieldset created successfully\n${fullpath}\n`);
-
-        var output = push_source(orgname);
-        console.log(output);
+        this.log(messages.getMessage('log.availableFields', [fields]));
+      }
     }
+    const prompt = await this.prompt<{response: string}>({
+      type: 'input',
+      name: 'response',
+      message: messages.getMessage('prompt.message.fields')
+    });
+    let fieldList = '';
+    prompt.response.split(',').forEach((elem) =>{
+      const displayFieldTemplate =
+      '   <displayedFields>\n'+
+      '       <field>{{fieldname}}</field>\n'+
+      '   </displayedFields>\n';
+      fieldList += displayFieldTemplate.replace(new RegExp('{{fieldname}}', 'g'), elem.trim());
+    });
+    this.content = this.content.replace(new RegExp('{{fieldlist}}', 'g'), fieldList);
+
+    // update content file
+    const fullpath = fsPath+'/'+apiname+'.fieldSet-meta.xml';
+    fs.writeFileSync(fullpath, this.content);
+    this.log(messages.getMessage('log.fieldsetCreated', [fullpath]));
+
+    const output = await this.pushSource(orgname, await flags['target-org']?.determineIfScratch(), fullpath);
+    return { output };
+  }
+  private async updateContent(varName: string, question: string): Promise<void> {
+    const prompt = await this.prompt<{response: string}>({
+      type: 'input',
+      name: 'response',
+      message: question
+    });
+
+    this.content = this.content.replace(new RegExp(`{{${varName}}}`, 'g'), prompt.response);
+  }
+
+  private async pushSource(orgname: string, usesScratchOrg: boolean|undefined, path: string): Promise<string>{
+    this.log('Push source to org...');
+    const command = usesScratchOrg
+    ? `sf project deploy start --ignore-warnings --ignore-conflicts --target-org ${orgname}`
+    : `sf project deploy start --ignore-warnings --ignore-conflicts --target-org ${orgname} --source-directory ${path}`;
+    try{
+      return await new Promise((resolve) => resolve(exec(command).toString()));
+    } catch(err){
+      throw messages.createError('error.pushFailed');
+    }
+  }
+
+  private async retrievesobjectfields(orgname: string, sobject: string): Promise<DescribeSObjectResult>{
+    this.log(messages.getMessage('log.getFields', [sobject, orgname]));
+    return this.connection!.describe(sobject);
+  }
 }

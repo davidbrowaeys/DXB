752734fea791d8e37df9f8fa9078468905ae5455
Olivier Brabanders <obrabanders@deloitte.com>
2023-10-10 15:25:32 +0200

    Migrate permissionset:create

---
+++ messages/permissionset.create.md
@@ -0,0 +1,41 @@
+# summary
+
+Create a permission set from a profile without layout and category group visiblity
+
+# examples
+
+- Create a named permissionset from an existing profile
+ 
+ <%= config.bin %> <%= command.id %> --profile force-app/main/default/profile/Admin.profile-meta.xml --permissionset-name AdminPermissionSet
+
+# flags.profile.summary
+
+File path of profile to create the permission set from
+
+# flags.permissionset-name.summary
+
+Name of the permision set to create
+
+# flags.output-dir.summary
+
+Output path of the permision set to create
+
+# flags.has-activation-required.summary
+
+Indicates whether the permission set requires an associated active session or not. This field is available in API version 53.0 and later.
+
+# flags.license.summary
+
+License name of the permision set to create
+
+# flags.description.summary
+
+Description of the permission set to create
+
+# log.description
+
+Permission Set created from %s
+
+# log.success
+
+Permissionset created: %s
--- src/commands/dxb/permissionset/create.ts
+++ src/commands/dxb/permissionset/create.ts
@@ -1,103 +1,93 @@
-import {
-    flags,
-    SfdxCommand
-} from '@salesforce/command';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
+import {Flags, SfCommand } from '@salesforce/sf-plugins-core';
 import * as xml2js from 'xml2js';
 import * as js2xmlparser from 'js2xmlparser';
+import { Messages } from '@salesforce/core';
+type PermissionSetCreateResult = {
+  path: string;
+}
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('dxb', 'permissionset.create');
+export default class PermissionSetCreate extends SfCommand<PermissionSetCreateResult> {
 
-export default class PermissionSetCreate extends SfdxCommand {
+  public static readonly summary = messages.getMessage('summary');
 
-    public static description = 'This command create a permission set from a profile without layotu and category group visiblity"';
+  public static readonly examples = messages.getMessages('examples');
 
-    public static examples = [
-        `$  sfdx dxb:permissionset:create -p force-app/main/default/profile/Admin.profile-meta.xml -n AdminPermissionSet`
-    ];
+  public static readonly flags = {
+    profile: Flags.file({
+      char: 'p',
+      summary: messages.getMessage('flags.profile.summary'),
+      required:true,
+      exists: true
+    }),
+    'permissionset-name': Flags.string({
+      char: 'n',
+      summary: messages.getMessage('flags.permissionset-name.summary'),
+      required:true
+    }),
+    'output-dir': Flags.directory({
+      char: 'r',
+      summary: messages.getMessage('flags.output-dir.summary'),
+      default: 'force-app/main/default/permissionsets'
+    }),
+    'has-activation-required': Flags.boolean({
+      char: 'a',
+      summary: messages.getMessage('flags.has-activation-required.summary'),
+      default: false
+    }),
+    license: Flags.string({
+      char: 'l',
+      summary: messages.getMessage('flags.license.summary'),
+      default: 'Salesforce'
+    }),
+    description: Flags.string({
+      char: 'd',
+      summary: messages.getMessage('flags.description.summary'),
+    })
+  };
 
-    public static args = [{
-        name: 'file'
-    }];
+  public async run(): Promise<PermissionSetCreateResult> {
+    const {flags} = await this.parse(PermissionSetCreate);
+    try{
+      const permissionSetPath = `${flags['output-dir']}/${flags['permissionset-name']}.permissionset-meta.xml`;
+      const data = await fs.readFile(flags.profile, {encoding: 'utf8'});
+      const profileName = flags.profile.split('/');
+      const result = (await xml2js.parseStringPromise(data, {
+        explicitArray: false,
+      }))?.Profile;
+      const permissionSet: any = {
+        label: flags['permissionset-name'],
+        hasActivationRequired:flags['has-activation-required'],
+        description: messages.getMessage('log.description',[profileName[profileName.length - 1]])
+      };
+      if (flags.license) permissionSet.license = flags.license;
+      if (flags.description) permissionSet.description = flags.description;
+      if (result.applicationVisibilities) permissionSet.applicationVisibilities = result.applicationVisibilities;
+      if (result.classAccesses) permissionSet.classAccesses = result.classAccesses;
+      if (result.customMetadataTypeAccesses) permissionSet.customMetadataTypeAccesses = result.customMetadataTypeAccesses;
+      if (result.customPermissions) permissionSet.customPermissions = result.customPermissions;
+      if (result.customSettingAccesses) permissionSet.customSettingAccesses = result.customSettingAccesses;
+      if (result.externalDataSourceAccesses) permissionSet.externalDataSourceAccesses = result.externalDataSourceAccesses;
+      if (result.flowAccesses) permissionSet.flowAccesses = result.flowAccesses;
+      if (result.objectPermissions) permissionSet.objectPermissions = result.objectPermissions;
+      if (result.fieldPermissions) permissionSet.fieldPermissions = result.fieldPermissions;
+      if (result.recordTypeVisibilities) permissionSet.recordTypeVisibilities = result.recordTypeVisibilities;
+      if (result.pageAccesses) permissionSet.pageAccesses = result.pageAccesses;
+      if (result.tabVisibilities) permissionSet.tabVisibilities = result.tabVisibilities;
+      if (result.userPermissions) permissionSet.userPermissions = result.userPermissions;
 
-    protected static flagsConfig = {
-        profile: flags.string({
-            char: 'p',
-            description: 'File path pf profile to create the permission set from',
-            required:true
-        }),
-        permissionsetname: flags.string({
-            char: 'n',
-            description: 'Name of the permision set to create',
-            required:true
-        }),
-        outputdir: flags.string({
-            char: 'o',
-            description: 'Output path of the permision set to create',
-            default: 'force-app/main/default/permissionsets'
-        }),
-        hasactivationrequired: flags.boolean({
-            char: 'a',
-            description: 'Indicates whether the permission set requires an associated active session (true) or not (false). The default value is false. This field is available in API version 53.0 and later.',
-            default: false
-        }),
-        license: flags.string({
-            char: 'l',
-            description: 'License name of thep permision set to create',
-            default: 'Salesforce'
-        }),
-        description: flags.string({
-            char: 'd',
-            description: 'Description of the permission set to create',
-        })
-    };
-    // Comment this out if your command does not require an org username
-    protected static requiresUsername = true;
+      const xml = js2xmlparser.parse('PermissionSet', permissionSet, {
+        declaration: {
+          encoding: 'UTF-8'
+        },
+      });
 
-    // Comment this out if your command does not support a hub org username
-    protected static supportsDevhubUsername = true;
-
-    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-    protected static requiresProject = false;
-
-    public async run() {
-        const {profile, permissionsetname, outputdir,hasactivationrequired, license, description} = this.flags;
-        try{
-            const permissionSetPath = `${outputdir}/${permissionsetname}.permissionset-meta.xml`;
-            const data = await fs.promises.readFile(profile, "utf8");
-            const profileName = profile.split('/');
-            const result = (await xml2js.parseStringPromise(data, {
-                explicitArray: false,
-            }))?.Profile;
-            let permissionSet:any = {
-                label: permissionsetname,
-                hasActivationRequired:hasactivationrequired,
-                description: `Permission Set created from ${profileName[profileName.length - 1]}`
-            };
-            if (license) permissionSet.license = license;
-            if (description) permissionSet.description = description;
-            if (result.applicationVisibilities) permissionSet.applicationVisibilities = result.applicationVisibilities;
-            if (result.classAccesses) permissionSet.classAccesses = result.classAccesses;
-            if (result.customMetadataTypeAccesses) permissionSet.customMetadataTypeAccesses = result.customMetadataTypeAccesses;
-            if (result.customPermissions) permissionSet.customPermissions = result.customPermissions;
-            if (result.customSettingAccesses) permissionSet.customSettingAccesses = result.customSettingAccesses;
-            if (result.externalDataSourceAccesses) permissionSet.externalDataSourceAccesses = result.externalDataSourceAccesses;
-            if (result.flowAccesses) permissionSet.flowAccesses = result.flowAccesses;
-            if (result.objectPermissions) permissionSet.objectPermissions = result.objectPermissions;
-            if (result.fieldPermissions) permissionSet.fieldPermissions = result.fieldPermissions;
-            if (result.recordTypeVisibilities) permissionSet.recordTypeVisibilities = result.recordTypeVisibilities;
-            if (result.pageAccesses) permissionSet.pageAccesses = result.pageAccesses;
-            if (result.tabVisibilities) permissionSet.tabVisibilities = result.tabVisibilities;
-            if (result.userPermissions) permissionSet.userPermissions = result.userPermissions;
-
-            const xml = js2xmlparser.parse("PermissionSet", permissionSet, {
-                declaration: {
-                    encoding: "UTF-8"
-                },
-            });
-
-            await fs.promises.writeFile(permissionSetPath, xml);
-            console.log(`Permissionsets(s) created: ${permissionSetPath}`);
-        }catch(err){
-            console.error(err);
-        }
+      await fs.writeFile(permissionSetPath, xml);
+      this.log(messages.getMessage('log.success', [permissionSetPath]));
+      return { path: permissionSetPath };
+    }catch(err: any){
+      this.error(err);
     }
+  }
 }
